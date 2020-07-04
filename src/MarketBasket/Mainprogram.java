696e0532a68abc5271f3bcb969dfbe4c3583d1bc
VDBay <azharsubhan97@gmail.com>
2020-07-04 16:00:19 +0700

    update combination

--- src/MarketBasket/Mainprogram.java
+++ src/MarketBasket/Mainprogram.java
@@ -4,19 +4,23 @@
  * Date :
  */
 package MarketBasket;
-
+import java.util.*;
 /**
  *
  * @author Acer
  */
-public class Mainprogram {
+public class Mainprogram extends Combination{
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
+        int arr[] = {1, 2, 3}; 
+	int r; 
+	int n = arr.length; 
+        for(r=1;r<=n;r++)
+	printCombination(arr, n, r);
     }
     
 }
