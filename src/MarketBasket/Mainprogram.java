3f9415a0b437160d1b06463f4107d6a5a1d4ca0e
VDBay <azharsubhan97@gmail.com>
2020-07-09 08:13:04 +0700

    update csv format

--- src/MarketBasket/Mainprogram.java
+++ src/MarketBasket/Mainprogram.java
@@ -19,18 +19,14 @@ public class Mainprogram extends Combination{
         int rand_int;
 
         FileWriter csvWriter = new FileWriter("DataTransaction.csv");
-        csvWriter.append("Pelanggan");
-        csvWriter.append(",");
-        csvWriter.append("Orderan");
+        csvWriter.append("Transaksi");
         csvWriter.append("\n");
 
         for (int i=1;i<=jumlahtransaksi;i++) {
-            csvWriter.append(Integer.toString(i));
-            csvWriter.append(", ");
             for(int j=1;j<=jumlahproduk;j++){
                 rand_int=(rand.nextInt(jumlahproduk)%jumlahproduk)+1;
                 csvWriter.append(Integer.toString(rand_int));
-                csvWriter.append("; ");
+                csvWriter.append(", ");
             }
             csvWriter.append("\n");
         }
